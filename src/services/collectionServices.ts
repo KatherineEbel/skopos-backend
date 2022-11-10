4cf0aa322a406c03d638b7677af057a86bf20c01
Katherine Ebel <kathyebel@me.com>
2022-11-10 08:49:14 -0500

    add code from previous repo
    Co-authored-by: Gagan Sapkota <here.gagan@gmail.com>
    Co-authored-by: Nykaela Dodson <nykaelad@gmail.com>
    Co-authored-by: Hans Elde <hanserik.elde@gmail.com>

---
+++ src/services/collectionServices.ts
@@ -0,0 +1,15 @@
+// import { PrismaClient } from '@prisma/client'
+// const prismaClient = new PrismaClient()
+
+// export const getCollectionData = async (collectionId) => {
+//   return prismaClient.request.findMany({
+//     where: {
+//       collectionId: {
+//         equals: collectionId,
+//       },
+//     },
+//     include: {
+//       assertions: true,
+//     },
+//   })
+// }
